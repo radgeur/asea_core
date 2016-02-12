af1d82836e615792a46642e7906067481fb0b156
radgeur <remy2112@hotmail.fr>
2016-02-12 11:02:20 +0100

    count core finish

--- main.c
+++ main.c
@@ -2,24 +2,27 @@
 #include <stdio.h>
 #include "./core/include/hardware.h"
 
+#define CORE_STATUS 0x80
+#define CORE_ID 0x126
+
 void countByCore(){
     int i;
-    printf("The current core is the number : %d\n", _in(0x126));
+    printf("The current core is the number : %d\n", _in(CORE_ID));
     for(i=0;i<(1<<20);i++){}
+    printf("The core %d is finish\n", _in(CORE_ID));
 }
 
 int main() {
-    int tab_core;
     if(init_hardware("./core/etc/core.ini") == 0) {
         fprintf(stderr, "Error in core initialization\n");
 	exit(EXIT_FAILURE);
     }
-   
-    _out(0x80,2 & 0xFF);
-    tab_core = _in(0x80);
-    printf("%d\n", tab_core);
 
-    IRQVECTOR[0] = countByCore();
+    IRQVECTOR[0] = countByCore;
+    
+    _out(CORE_STATUS,255);
+
+    while(1){}
 
     return 1;
 }
